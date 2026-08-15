2bd5a4cc973f3e9d931f682250d0ecae1f1e2b76
Bump.yml <ghactions@anyvm.org>
2026-08-15 11:19:33 +0000

    Bump builder versions [testos=ubuntu]

--- anyvm.py
+++ anyvm.py
@@ -152,7 +152,7 @@ DEFAULT_BUILDER_VERSIONS = {
     "midnightbsd": "2.0.6",
     "tribblix": "2.0.6",
     "openindiana": "2.1.1",
-    "ubuntu": "2.0.8",
+    "ubuntu": "2.0.9",
     "openeuler": "2.0.2",
     "ghostbsd": "2.0.7",
     "blissos": "2.0.2",
